fbe26d4e400e3379a6403760d0e7f45a21fb5383
CodeSolaris <aleksandr.volkov.it@gmail.com>
2023-11-09 13:59:46 +0200

    made minor fixes

--- main.py
+++ main.py
@@ -1,30 +1,53 @@
 import streamlit as st
 
-col1, col2 = st.columns(2)
+# Set page configuration
+st.set_page_config(
+    page_title="Aleksandr's Portfolio",
+    layout="wide",
+)
 
-with col1:
-    st.image("images/photo.png")
+# Create two columns
+left_column, right_column = st.columns(2)
 
-with col2:
+# Left column
+with left_column:
+    st.write("")
+    st.write("")
+    st.image("images/photo.png", use_column_width=True)
+
+# Right column
+with right_column:
     st.title("Hi, I'm Aleksandr")
     content = """
-Hello, my name is Aleksandr. I am an aspiring developer with immense enthusiasm and
-a passion for creating innovative software solutions. My journey into the realm of 
-programming began with a fascination for technology and a keen desire 
-to grow in this field.
-My primary areas of interest encompass [list the areas of development that interest you,
-for example, web development, mobile development, artificial intelligence, databases, etc.].
-I am actively studying [mention specific technologies, programming languages, 
- or frameworks] to expand my skills and reach new heights in development.
-Within my portfolio, you'll find several projects I've worked on. Even at the initial
-stage of my career, I'm confident that my projects reflect professionalism,
-a commitment to quality, and an ongoing drive to enhance my skills.
-I also aim to engage in teaching and sharing knowledge within the developer community.
-I believe that interacting with other professionals in the field not only deepens my 
-knowledge but also helps me grow as a professional.
-I take pride in being part of this exciting industry and strive to become a more competent 
-and experienced developer in the future.
-Thank you for your interest in my portfolio, and I hope for the opportunity to 
-contribute to projects that benefit society and create new avenues for innovation.
+<p>I'm a passionate developer enthusiastic about crafting web and desktop applications,
+ website scraping, and Telegram bots.
+My journey into programming began with a fascination for technology and a desire to 
+create innovative solutions in these domains.</p>
+<p>I'm particularly drawn to the development of web and desktop applications
+ finding joy in building intuitive and user-friendly interfaces.
+Additionally, I've found interest in the art of web scraping, extracting valuable 
+information from websites, and leveraging it for various purposes.
+Moreover, exploring the realm of Telegram bots has been an exciting avenue, 
+allowing me to delve into chatbot development and automation within 
+this popular platform.</p>
+<p>My portfolio showcases projects where I've applied my skills,
+even at this early stage of my career. 
+These projects reflect my commitment to precision,
+a pursuit of excellence, and an unwavering dedication to continuous 
+learning and improvement.</p>
+<p>I'm also keen on engaging with the developer community, fostering collaboration, 
+and sharing knowledge. I strongly believe that interacting with other
+experts in the field enhances not only my knowledge 
+but also my growth as a professional.</p>
+<p>Being a part of the dynamic world of technology fills me with immense pride. 
+My goal is to continue evolving as a skilled and experienced developer
+while contributing meaningfully to innovative projects that make a difference.</p>
+<p>Thank you for your interest in exploring my portfolio. 
+I'm excited about the prospect of contributing to projects that push the boundaries
+ of innovation and have a positive impact.</p>
 """
-st.write(content)
+    st.write(content, unsafe_allow_html=True)
+
+st.write(
+    "Below you can find some of the apps I have built in Python.Feel free to contact me!"
+)
